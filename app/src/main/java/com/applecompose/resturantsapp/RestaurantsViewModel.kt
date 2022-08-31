55bdd4ff64a7fa56d58f071a7e9648cc5f0ba5a7
Jerry applegarth <applegarth1630@gmail.com>
2022-08-31 14:10:12 -0700

    1.4 state is now in the view model

--- app/src/main/java/com/applecompose/resturantsapp/RestaurantsViewModel.kt
+++ app/src/main/java/com/applecompose/resturantsapp/RestaurantsViewModel.kt
@@ -10,7 +10,18 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class RestaurantsViewModel(): ViewModel() {
-	fun getRestaurants() = dummyRestaurants
+	val state = mutableStateOf(dummyRestaurants)
+
+	fun toggleFavorite(id: Int) {
+		val restaurants = state.value.toMutableList()
+		val itemIndex =
+			restaurants.indexOfFirst { it.id == id }
+		val item = restaurants[itemIndex]
+		restaurants[itemIndex] =
+			item.copy(isFavorite = !item.isFavorite)
+		state.value = restaurants
+	}
+
 
 
 }
