4d662fad5b922815ac4c1d90f8936ff8b2ae001c
Jerry applegarth <applegarth1630@gmail.com>
2022-08-31 13:15:26 -0700

    1.2 Hoisting State

--- app/src/main/java/com/applecompose/resturantsapp/RestaurantsScreen.kt
+++ app/src/main/java/com/applecompose/resturantsapp/RestaurantsScreen.kt
@@ -1,6 +1,7 @@
 package com.applecompose.resturantsapp.presentation.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -9,9 +10,11 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Place
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -20,7 +23,6 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.applecompose.resturantsapp.Restaurant
 import com.applecompose.resturantsapp.RestaurantsViewModel
-import com.applecompose.resturantsapp.dummyRestaurants
 
 @Composable
 fun RestaurantsScreen() {
@@ -40,6 +42,8 @@ fun RestaurantsScreen() {
 
 @Composable
 fun RestaurantItem(item: Restaurant) {
+	var favoriteState = remember { mutableStateOf(false) }
+	val icon = if (favoriteState.value) Icons.Filled.Favorite else Icons.Filled.FavoriteBorder
 	Card(
 		elevation = 4.dp,
 		modifier = Modifier.padding(8.dp)
@@ -48,10 +52,42 @@ fun RestaurantItem(item: Restaurant) {
 			verticalAlignment = Alignment.CenterVertically,
 			modifier = Modifier.padding(8.dp)
 		) {
-			RestaurantIcon(Icons.Filled.Place, Modifier.weight(0.15f))
-			RestaurantDetails(item.title, item.description, Modifier.weight(0.85f))
+			RestaurantIcon(
+				Icons.Filled.Place,
+				Modifier.weight(0.15f)
+			)
+			RestaurantDetails(
+				item.title,
+				item.description,
+				Modifier.weight(0.7f)
+			)
+			FavoriteIcon(
+				icon,
+				Modifier.weight(0.15f)
+			) {
+				favoriteState.value =
+					!favoriteState.value
+			}
+
 		}
 	}
+}
+
+@Composable
+fun FavoriteIcon(
+	icon: ImageVector,
+	modifier: Modifier,
+	onClick: () -> Unit
+) {
+
+	Image(
+		imageVector = icon,
+		contentDescription = "Favorite restaurant icon",
+		modifier = Modifier
+			.padding(8.dp)
+			.clickable { onClick() }
+	)
+
 }
 
 @Composable
--- app/src/main/java/com/applecompose/resturantsapp/RestaurantsViewModel.kt
+++ app/src/main/java/com/applecompose/resturantsapp/RestaurantsViewModel.kt
@@ -1,7 +1,16 @@
 package com.applecompose.resturantsapp
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class RestaurantsViewModel(): ViewModel() {
 	fun getRestaurants() = dummyRestaurants
+
+
 }
